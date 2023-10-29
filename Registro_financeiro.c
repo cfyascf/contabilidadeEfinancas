036657a7a8870afe723609c9a1764c4d1d9b2c72
Juan Diego <juandiego.jd42@Gmail.com>
2023-10-29 20:53:33 -0300

    a

---
+++ Registro_financeiro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct registro{
+    id int,
+    tipo char(50),
+    de char(50),
+    para char(50),
+    data char(11),
+    valor float,
+};
+ 
+
+ int main(){
+
+
+    return 0;
+ }
