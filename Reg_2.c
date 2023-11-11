97991fdb1ff6de3465791320ed3f9b9b7689e97e
Juan Diego <juandiego.jd42@Gmail.com>
2023-11-11 15:47:15 -0300

    aaaaa

--- Reg_2.c
+++ Reg_2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <locale.h>
+#include <string.h>
 
 struct reg{
     int regid ;
@@ -28,23 +29,33 @@ void cad_fin (int c){
     gets(regi.data);
     printf("Escreva o valor da operação financeira ");
     scanf("%f",&regi.valor);
-    fprintf(arq,"/ %i / %s / %s / %s / %s / %.2f \n",c,regi.tipo,regi.para,regi.de,regi.data,regi.valor);
+    fprintf(arq," %i / %s / %s / %s / %s / %.2f \n",c,regi.tipo,regi.para,regi.de,regi.data,regi.valor);
     fclose(arq);
 };
 void con_fin(int c){
     printf("############################ CONSULTA DE OPERAÇÔES FINANCEIRA ##################");
-    struct reg regi;
-    int j;
     char linha[100];
     FILE * arq = fopen("arquivo.txt", "r");
     while (fgets(linha,sizeof(linha),arq) != NULL){
-        fscanf(arq,"/ %i / %d / %d / %d / %d / %f ",&j,&regi.tipo,&regi.para,&regi.de,&regi.data,&regi.valor);
-        printf("%s",linha);
-        printf("/ %i / %d / %d / %d / %d / %f",j,regi.tipo,regi.para,regi.de,regi.data,regi.valor);
+        printf("\n %s",linha);
     };
     printf("\n O total de consutas é %i \n",c);
     fclose(arq);
 };
+void pesq_id(){
+    char peq[100],linha[100];
+    FILE * arq = fopen("arquivo.txt","r");
+    printf("##################### PESQUISA POR ID ##########################");
+    printf("\nDIGITE O ID PARA PESQUISAR: ");
+    fflush(stdin);
+    gets(peq);
+    while (fgets(linha,sizeof(linha),arq) != NULL){
+        if (strstr(linha,peq) != NULL){
+            printf("\n %s",linha);
+        }
+    };
+
+}
 int main(){
     setlocale (LC_ALL,"PORTUGUESE");
     int m=-1,c;
@@ -58,13 +69,15 @@ int main(){
     };
     fclose(arq2);            
     while(m != 0){
-        printf("\n ############### MENU #################### \n Escreva 1 para cadastrar Operações financeiras \n Escreva 2 para consutar Operações financeiras \n Escreva 0 para sair \n");
+        printf("\n ############### MENU #################### \n Escreva 1 para cadastrar Operações financeiras \n Escreva 2 para consutar Operações financeiras \n Escreva 3 para pesquisar Operações financeiras por ID \n Escreva 0 para sair \n");
         scanf("%i",&m);
         if (m == 1){
             c++;
             cad_fin(c);
         }else if(m == 2){
             con_fin(c);
+        }else if(m == 3){   
+            pesq_id();
         }else if(m == 0){
             FILE*arq2=fopen("arquivo1.txt","w");
             fprintf(arq2," %i ", c);
