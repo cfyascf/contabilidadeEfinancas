0209992937ab288c06a72cd5b8bceea7922227e3
Juan Diego <juandiego.jd42@Gmail.com>
2023-11-12 13:14:42 -0300

    alguns mods

--- Registro_financeiro.c
+++ Registro_financeiro.c
@@ -33,12 +33,21 @@ void cad_fin (int c){
     fclose(arq);
 };
 void con_fin(int c){
-    printf("############################ CONSULTA DE OPERAÇÔES FINANCEIRA ##################");
-    char linha[100];
+    printf("############################ CONSULTA DE OPERAÇÔES FINANCEIRA ################## \n");
+    char **linha = NULL,contd[100];
+    int c_linha=0,i;
     FILE * arq = fopen("arquivo.txt", "r");
-    while (fgets(linha,sizeof(linha),arq) != NULL){
-        printf("%s",linha);
+    while( fgets(contd,sizeof(contd),arq) !=NULL){
+    c_linha++;
+    linha = realloc (linha, c_linha * sizeof(char*));
+    linha[c_linha -1]= malloc (strlen(contd)+1);
+    strcpy(linha[c_linha-1],contd);
     };
+    for (i=0 ; i< c_linha ; i++){
+        if (i != 0){
+            printf("%s",linha[i]);
+        }
+    }
     printf("\n O total de consutas é %i \n",c);
     fclose(arq);
 };
@@ -56,7 +65,7 @@ void pesq_id(){
     printf("\n DIGITE O ID DA PESQUISA ");
     fflush(stdin);
     gets(contd);
-    for ( i=0;i < c_linha; i+=7 ){
+    for ( i=1;i < c_linha; i+=7 ){
         if (strstr(linha[i],contd) != NULL){
             printf ("%s",linha[i]);
             printf ("%s",linha[i+1]);
@@ -86,7 +95,7 @@ void pesq_tipo(){
     printf("\n DIGITE O TIPO: ");
     fflush(stdin);
     gets(contd);
-    for ( i=1;i < c_linha; i+=7 ){
+    for ( i=2;i < c_linha; i+=7 ){
         if (strstr(linha[i],contd) != NULL){
             printf ("%s",linha[i-1]);
             printf ("%s",linha[i]);
@@ -116,7 +125,7 @@ void pesq_rece(){
     printf("\n DIGITE O RECEBEDOR: ");
     fflush(stdin);
     gets(contd);
-    for ( i=2;i < c_linha; i+=7 ){
+    for ( i=3;i < c_linha; i+=7 ){
         if (strstr(linha[i],contd) != NULL){
             printf ("%s",linha[i-2]);
             printf ("%s",linha[i-1]);
@@ -146,7 +155,7 @@ void pesq_paga(){
     printf("\n DIGITE O PAGADOR: ");
     fflush(stdin);
     gets(contd);
-    for ( i=3;i < c_linha; i+=7 ){
+    for ( i=4;i < c_linha; i+=7 ){
         if (strstr(linha[i],contd) != NULL){
             printf ("%s",linha[i-3]);
             printf ("%s",linha[i-2]);
@@ -162,10 +171,8 @@ void pesq_paga(){
         printf("Pagador não encontrado");
     };
 };
-void pesq_data(){
-    printf("############################# PESQUISAR POR DATA ############################");
-    char **linha = NULL,contd[100];
-    int c_linha=0,i,flag=0;
+void pont_cha(char **linha, int c_linha){
+    char contd[100];
     FILE * arq = fopen("arquivo.txt","r");
     while( fgets(contd,sizeof(contd),arq) !=NULL){
         c_linha++;
@@ -173,10 +180,24 @@ void pesq_data(){
         linha[c_linha -1]= malloc (strlen(contd)+1);
         strcpy(linha[c_linha-1],contd);
     };
+    fclose(arq);
+};
+void pesq_data(){
+    printf("############################# PESQUISAR POR DATA ############################");
+    char **linha = NULL,contd[100];
+    int c_linha=0,i,flag=0;
+    // FILE * arq = fopen("arquivo.txt","r");
+    // while( fgets(contd,sizeof(contd),arq) !=NULL){
+    //     c_linha++;
+    //     linha = realloc (linha, c_linha * sizeof(char*));
+    //     linha[c_linha -1]= malloc (strlen(contd)+1);
+    //     strcpy(linha[c_linha-1],contd);
+    // };
+    pont_cha(**linha,c_linha);
     printf("\n DIGITE A DATA: ");
     fflush(stdin);
     gets(contd);
-    for ( i=4;i < c_linha; i+=7 ){
+    for ( i=5;i < c_linha; i+=7 ){
         if (strstr(linha[i],contd) != NULL){
             printf ("%s",linha[i-4]);
             printf ("%s",linha[i-3]);
@@ -187,7 +208,7 @@ void pesq_data(){
             flag++;
         };
     };
-    fclose(arq);
+    // fclose(arq);
     if (flag==0){
         printf("Data não encontrado");
     };
@@ -206,7 +227,7 @@ void pesq_valor(){
     printf("\n DIGITE O VALOR: ");
     fflush(stdin);
     gets(contd);
-    for ( i=5;i < c_linha; i+=7 ){
+    for ( i=6;i < c_linha; i+=7 ){
         if (strstr(linha[i],contd) != NULL){
             printf ("%s",linha[i-5]);
             printf ("%s",linha[i-4]);
@@ -221,19 +242,30 @@ void pesq_valor(){
     if (flag==0){
         printf("Valor não encontrado");
     };
-}
+};
+int desc_id_r(){
+    int c;
+    char linha[100];
+    FILE*arq=fopen("arquivo.txt","r");
+    if (arq == NULL){
+        c=0;
+    }else{
+        fgets(linha,100,arq);
+        c=atoi(linha);
+
+    };
+    fclose(arq); 
+    return c;
+};
+void salvar_id(int c){
+            FILE*arq=fopen("arquivo.txt","r+");
+            fprintf(arq," %i ", c);
+            fclose(arq); 
+};
 int main(){
     setlocale (LC_ALL,"PORTUGUESE");
     int m=-1,c;
-    char linha[100];
-    FILE*arq2=fopen("arquivo1.txt","r");
-    if (arq2 == NULL){
-        c=0;
-    }else{
-        fgets(linha,100,arq2);
-        c=atoi(linha);
-    };
-    fclose(arq2);            
+    c = desc_id_r();          
     while(m != 0){
         printf("\n ############### MENU #################### \n Escreva 1 para cadastrar Operações financeiras \n Escreva 2 para consutar Operações financeiras \n Escreva 3 para pesquisar por ID \n Escreva 4 para pesquisar por tipo \n Escreva 5 para pesquisar por recebedor \n Escreva 6 para pesquisar por pagador \n Escreva 7 para pesquisar por data \n Escreva 8 para pesquisar por valor \n Escreva 0 para sair \n");
         scanf("%i",&m);
@@ -254,10 +286,8 @@ int main(){
             pesq_data();
         }else if (m == 8){
             pesq_valor();
-        }else if(m == 0){
-            FILE*arq2=fopen("arquivo1.txt","w");
-            fprintf(arq2," %i ", c);
-            fclose(arq2);                          
+        }else if(m == 0){    
+            salvar_id(c);                     
             return 0;
         };
     };
